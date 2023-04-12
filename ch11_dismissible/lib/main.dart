bea1e5603f4ab64cd1da2b610b14883666e3b6de
Kinh Chen <kinhchendev@gmail.com>
2023-04-12 11:09:54 +0700

    complete ch11_dismissible project

--- ch11_dismissible/lib/main.dart
+++ ch11_dismissible/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ch11_dismissible/classes/trip.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Home(),
     );
   }
 }
@@ -37,8 +38,105 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final List _trips = <Trip>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _trips..add(Trip(id: '0', tripName: 'Rome', tripLocation: 'Italy'))
+      ..add(Trip(id: '1', tripName: 'Paris', tripLocation: 'France'))
+      ..add(Trip(id: '2', tripName: 'New York', tripLocation: 'USA - New York'))
+      ..add(Trip(id: '3', tripName: 'Cancun', tripLocation: 'Mexico'))
+      ..add(Trip(id: '4', tripName: 'London', tripLocation: 'England'))
+      ..add(Trip(id: '5', tripName: 'Sydney', tripLocation: 'Australia'))
+      ..add(Trip(id: '6', tripName: 'Miami', tripLocation: 'USA - Florida'))
+      ..add(Trip(id: '7', tripName: 'Rio de Janeiro', tripLocation: 'Brazil'))
+      ..add(Trip(id: '8', tripName: 'Cusco', tripLocation: 'Peru'))
+      ..add(Trip(id: '9', tripName: 'New Delhi', tripLocation: 'India'))
+      ..add(Trip(id: '10', tripName: 'Tokyo', tripLocation: 'Japan'));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dismissible'),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: _trips.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+              key: Key(_trips[index].id),
+              background: _buildCompleteTrip(),
+              secondaryBackground: _buildRemoveTrip(),
+              onDismissed: (DismissDirection direction) {
+                direction == DismissDirection.startToEnd ? _markTripCompleted() : _deleteTrip();
+                // Remove item from List
+                setState(() {
+                  _trips.removeAt(index);
+                });
+              },
+              child: _buildListTile(index),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  ListTile _buildListTile(int index) {
+    return ListTile(
+      title: Text('${_trips[index].tripName}'),
+      subtitle: Text('${_trips[index].tripLocation}'),
+      leading: Icon(Icons.flight),
+      trailing: Icon(Icons.fastfood),
+    );
+  }
+
+  Container _buildCompleteTrip() {
+    return Container(
+      color: Colors.green,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            Icon(
+              Icons.done,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _buildRemoveTrip() {
+    return Container(
+      color: Colors.red,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: const [
+            Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _markTripCompleted() {
+    // Mark trip completed in Database or web service
+
+  }
+
+  void _deleteTrip() {
+    // Delete trip from Database or web service
+
   }
 }
